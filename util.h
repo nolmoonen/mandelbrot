ddba0bf184831e9812423635506c7e33456cf6c0
nolmoonen <nolmoonen@hotmail.nl>
2019-05-10 23:01:20 +0200

    Fixed functions

--- util.h
+++ util.h
@@ -28,4 +28,39 @@ uint32_t max(uint32_t a, uint32_t b) {
     return a > b ? a : b;
 }
 
+static char *readFile(const char *filename) {
+    char *buffer = 0;
+    FILE *f = fopen(filename, "rb");
+
+    if (f) {
+        fseek(f, 0, SEEK_END);
+        long length = ftell(f);
+        fseek(f, 0, SEEK_SET);
+
+        buffer = malloc(length + 1);
+        if (buffer) {
+            fread(buffer, 1, length, f);
+            buffer[length] = '\0';
+        }
+        fclose(f);
+    } else {
+        fprintf(stderr, "util: could not open file\n");
+    }
+
+    return buffer;
+}
+
+static uint32_t fileSize(const char *filename) {
+    FILE *f = fopen(filename, "rb");
+
+    if (f) {
+        fseek(f, 0, SEEK_END);
+        return ftell(f);
+    } else {
+        fprintf(stderr, "util: could not open file\n");
+    }
+
+    return 0;
+}
+
 #endif //MANDELBROT_UTIL_H
--- vulkan.h
+++ vulkan.h
@@ -57,6 +57,8 @@ VkExtent2D swapChainExtent;
 
 VkImageView *swapChainImageViews;
 
+VkPipelineLayout pipelineLayout;
+
 struct QueueFamilyIndices {
     int graphicsFamilyHasValue;
     uint32_t graphicsFamily;
@@ -566,7 +568,7 @@ bool createImageViews() {
         createInfo.subresourceRange.layerCount = 1;
 
         if (vkCreateImageView(logicalDevice, &createInfo, NULL, &swapChainImageViews[i]) != VK_SUCCESS) {
-            fprintf(stderr, "failed to create image views!");
+            fprintf(stderr, "vulkan: failed to create image views");
             return false;
         }
     }
@@ -574,34 +576,165 @@ bool createImageViews() {
     return true;
 }
 
+VkShaderModule createShaderModule(const char *code, uint32_t codeSize) {
+    VkShaderModuleCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    createInfo.codeSize = codeSize;
+    createInfo.pCode = (const uint32_t *) (code);
+
+    VkShaderModule shaderModule;
+    if (vkCreateShaderModule(logicalDevice, &createInfo, NULL, &shaderModule) != VK_SUCCESS) {
+        fprintf(stderr, "vulkan: failed to create shader module");
+        return false;
+    }
+
+    return shaderModule;
+}
+
+bool createGraphicsPipeline() {
+    const char *vertShaderFileName = "../shaders/vert.spv";
+    const char *fragShaderFileName = "../shaders/frag.spv";
+
+    char *vertShaderCode = readFile(vertShaderFileName);
+    char *fragShaderCode = readFile(fragShaderFileName);
+
+    uint32_t vertShaderCodeSize = fileSize(vertShaderFileName);
+    uint32_t fragShaderCodeSize = fileSize(fragShaderFileName);
+
+    VkShaderModule vertShaderModule = createShaderModule(vertShaderCode, vertShaderCodeSize);
+    VkShaderModule fragShaderModule = createShaderModule(fragShaderCode, fragShaderCodeSize);
+
+    VkPipelineShaderStageCreateInfo vertShaderStageInfo = {};
+    vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    vertShaderStageInfo.module = vertShaderModule;
+    vertShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo fragShaderStageInfo = {};
+    fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    fragShaderStageInfo.module = fragShaderModule;
+    fragShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+
+    /*
+     * Vertex input
+     */
+
+    VkPipelineVertexInputStateCreateInfo vertexInputInfo = {};
+    vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+    vertexInputInfo.vertexBindingDescriptionCount = 0;
+    vertexInputInfo.vertexAttributeDescriptionCount = 0;
+
+    /*
+     * Input assembly
+     */
+
+    VkPipelineInputAssemblyStateCreateInfo inputAssembly = {};
+    inputAssembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+    inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+    inputAssembly.primitiveRestartEnable = VK_FALSE;
+
+    /*
+     * Viewport and scissor
+     */
+
+    VkViewport viewport = {};
+    viewport.x = 0.0f;
+    viewport.y = 0.0f;
+    viewport.width = (float) swapChainExtent.width;
+    viewport.height = (float) swapChainExtent.height;
+    viewport.minDepth = 0.0f;
+    viewport.maxDepth = 1.0f;
+
+    VkRect2D scissor = {};
+    VkOffset2D offset = {0, 0};
+    scissor.offset = offset;
+    scissor.extent = swapChainExtent;
+
+    VkPipelineViewportStateCreateInfo viewportState = {};
+    viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+    viewportState.viewportCount = 1;
+    viewportState.pViewports = &viewport;
+    viewportState.scissorCount = 1;
+    viewportState.pScissors = &scissor;
+
+    /*
+     * Rasterizer
+     */
+    VkPipelineRasterizationStateCreateInfo rasterizer = {};
+    rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    rasterizer.depthClampEnable = VK_FALSE;
+    rasterizer.rasterizerDiscardEnable = VK_FALSE;
+    rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
+    rasterizer.lineWidth = 1.0f;
+    rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+    rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    rasterizer.depthBiasEnable = VK_FALSE;
+    rasterizer.depthBiasConstantFactor = 0.0f; // Optional
+    rasterizer.depthBiasClamp = 0.0f; // Optional
+    rasterizer.depthBiasSlopeFactor = 0.0f; // Optional
+
+    /*
+     * Multisampling
+     */
+    VkPipelineMultisampleStateCreateInfo multisampling = {};
+    multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+    multisampling.sampleShadingEnable = VK_FALSE;
+    multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+    multisampling.minSampleShading = 1.0f; // Optional
+    multisampling.pSampleMask = NULL; // Optional
+    multisampling.alphaToCoverageEnable = VK_FALSE; // Optional
+    multisampling.alphaToOneEnable = VK_FALSE; // Optional
+
+    /*
+     * Color blending
+     */
+    VkPipelineColorBlendAttachmentState colorBlendAttachment = {};
+    colorBlendAttachment.colorWriteMask =
+            VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+    colorBlendAttachment.blendEnable = VK_FALSE;
+
+    VkPipelineColorBlendStateCreateInfo colorBlending = {};
+    colorBlending.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+    colorBlending.logicOpEnable = VK_FALSE;
+    colorBlending.logicOp = VK_LOGIC_OP_COPY;
+    colorBlending.attachmentCount = 1;
+    colorBlending.pAttachments = &colorBlendAttachment;
+    colorBlending.blendConstants[0] = 0.0f;
+    colorBlending.blendConstants[1] = 0.0f;
+    colorBlending.blendConstants[2] = 0.0f;
+    colorBlending.blendConstants[3] = 0.0f;
+
+    /*
+     * Pipe layout
+     */
+    VkPipelineLayoutCreateInfo pipelineLayoutInfo = {};
+    pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    pipelineLayoutInfo.setLayoutCount = 0;
+    pipelineLayoutInfo.pushConstantRangeCount = 0;
+
+    if (vkCreatePipelineLayout(logicalDevice, &pipelineLayoutInfo, NULL, &pipelineLayout) != VK_SUCCESS) {
+        fprintf(stdout, "vulkan: failed to create pipeline layout\n");
+        return false;
+    }
+
+    vkDestroyShaderModule(logicalDevice, fragShaderModule, NULL);
+    vkDestroyShaderModule(logicalDevice, vertShaderModule, NULL);
+
+    return true;
+}
+
 bool vulkanInit(GLFWwindow *window) {
-    if (!createInstance()) {
-        return false;
-    }
-
-    if (!setupDebugMessenger()) {
-        return false;
-    }
-
-    if (!createSurface(window)) {
-        return false;
-    }
-
-    if (!pickPhysicalDevice()) {
-        return false;
-    }
-
-    if (!createLogicalDevice()) {
-        return false;
-    }
-
-    if (!createSwapChain()) {
-        return false;
-    }
-
-    if (!createImageViews()) {
-        return false;
-    }
+    if (!createInstance()) return false;
+    if (!setupDebugMessenger()) return false;
+    if (!createSurface(window)) return false;
+    if (!pickPhysicalDevice()) return false;
+    if (!createLogicalDevice()) return false;
+    if (!createSwapChain()) return false;
+    if (!createImageViews()) return false;
+    if (!createGraphicsPipeline()) return false;
 
 //    getextensions();
 
@@ -609,6 +742,8 @@ bool vulkanInit(GLFWwindow *window) {
 }
 
 void vulkanTerminate() {
+    vkDestroyPipelineLayout(logicalDevice, pipelineLayout, NULL);
+
     for (uint32_t i = 0; i < nrof_swapChainImages; ++i) {
         VkImageView imageView = *(swapChainImageViews + i);
         vkDestroyImageView(logicalDevice, imageView, NULL);
